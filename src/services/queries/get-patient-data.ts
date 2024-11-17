690fddfcf0acdeca6ac072d780561184be8507af
Deiby Gomez <gokaju23@gmail.com>
2024-11-17 16:48:18 -0500

    Enhance get-patient-data query: Add additional joins for photo and signature annotations

--- src/services/queries/get-patient-data.ts
+++ src/services/queries/get-patient-data.ts
@@ -1,4 +1,7 @@
 export const getPatientDataByOrderQuery = `
+
+
+
 select 
 t.tick_fecharecepcion,
 t.tick_fechaprocesado,
@@ -18,10 +21,10 @@ emple.empl_firma as firma_medico,
 case when anotphoto.anot_foto is not null then anotphoto.anot_foto else  p.paci_foto end as photo_paciente,
 anotphoto.anot_edad as edad,
 case when hf.anot_huella is not null then hf.anot_huella else
-	case when hf_altern.anot_huella is not null then hf_altern.anot_huella else p.paci_huella end 
+	case when hf_alter.anot_huella is not null then hf_alter.anot_huella else p.paci_huella end 
 end as anot_huella,
 case when hf.anot_firma is not null then hf.anot_firma else
-	case when hf_altern.anot_firma is not null then hf_altern.anot_firma else p.paci_firma end 
+	case when hf_alter.anot_firma is not null then hf_alter.anot_firma else p.paci_firma end 
 end as anot_firma
 
 from javap.ticket t 
@@ -30,15 +33,31 @@ inner join javaphc.pacientes p on p.paci_id  = tick_paciente
 left join javap.entidades e on p.paci_eps = e.enti_id 
 left join javap.estadocivil ec on ec.esci_id = p.paci_ecivil
 left join javap.empleados emple on emple.empl_id = t.empl_idmedico
+-- photo anotation
+left join (
+		select   tcs.tick_id, anotphoto.*
+		from  javap.ticket_clienteservicio tcs 
+		inner join javaphc.anotaciones anotphoto on anotphoto.tics_id = tcs.tics_id and anotphoto.form_id =12 and anotphoto.anot_estado = 'FINALIZADO'
+		-- where  tcs.tick_id =
+) anotphoto on anotphoto.tick_id =  t.tick_id
 
-left join javap.ticket_clienteservicio tcs on tcs.tick_id = t.tick_id
+-- main signature 
 
-left join javaphc.anotaciones anotphoto on anotphoto.tics_id = tcs.tics_id and anotphoto.form_id =12
-left join javaphc.anotaciones anotsignature on anotsignature.tics_id = tcs.tics_id and anotsignature.form_id = 1
-left join javaphc.huellafirma hf on hf.anot_id =  anotsignature.anot_id
+left join (
+	select   tcs.tick_id, hf.*
+		from  javap.ticket_clienteservicio tcs 
+		inner join javaphc.anotaciones anotsignature on anotsignature.tics_id = tcs.tics_id and anotsignature.form_id =1 and anotsignature.anot_estado = 'FINALIZADO'
+		inner join javaphc.huellafirma hf on hf.anot_id =  anotsignature.anot_id
+) hf on hf.tick_id =  t.tick_id
 
-left join javaphc.anotaciones anotsignature_altern on anotsignature_altern.tics_id = tcs.tics_id and anotsignature_altern.form_id = 141
-left join javaphc.huellafirma hf_altern on hf_altern.anot_id =  anotsignature_altern.anot_id
+-- alter signature
+
+left join (
+	select   tcs.tick_id, hf.*
+		from  javap.ticket_clienteservicio tcs 
+		inner join javaphc.anotaciones anotsignature on anotsignature.tics_id = tcs.tics_id and anotsignature.form_id =141 and anotsignature.anot_estado = 'FINALIZADO'
+		inner join javaphc.huellafirma hf on hf.anot_id =  anotsignature.anot_id
+) hf_alter on hf_alter.tick_id =  t.tick_id
 
 where t.tick_id =  ?
 `;
